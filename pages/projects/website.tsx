b5b36ea97ba7f59c9e3fecc2054a0f50f1f9c775
eZaplachi <evan@zaplachi.ca>
2021-12-10 02:06:59 -0700

    added contentful

--- pages/projects/website.tsx
+++ pages/projects/website.tsx
@@ -1,13 +1,55 @@
 import Layout from "../../components/Layout";
 import styles from "../../styles/pages/projects/website.module.css";
+import { GraphQLClient, gql } from "graphql-request";
 
-const Website = () => {
+export async function getStaticProps() {
+  const endpoint = `https://graphql.contentful.com/content/v1/spaces/${process.env.SPACE_ID}`;
+
+  const graphQLClient = new GraphQLClient(endpoint, {
+    headers: {
+      authorization: `Bearer ${process.env.CDA_TOKEN}`,
+    },
+  });
+
+  const query = gql`
+    {
+      projectsCollection {
+        items {
+          name
+          description
+          thumbnail {
+            title
+            description
+            contentType
+            fileName
+            size
+            url
+            width
+            height
+          }
+        }
+      }
+    }
+  `;
+
+  const projects = await graphQLClient.request(query);
+
+  return {
+    props: { projects }
+  }
+}
+
+getStaticProps().catch((error) => console.error(error));
+
+const Website = ({ projects }: any) => {
+  console.log(projects)
+
   return (
-    <main>
-      <Layout footerText='my intro to webdev' stickyOffset={0}>
+    <div>
+      <Layout footerText="my intro to webdev" stickyOffset={0}>
         <body className={styles.main}></body>
       </Layout>
-    </main>
+    </div>
   );
 };
 
