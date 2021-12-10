6aa51fc77ab54f6e90b19c1c7706fbdc4473a352
eZaplachi <evan@zaplachi.ca>
2021-12-10 00:47:34 -0700

    Updated dependancies -- broke mailer perms

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image"
-import Fade from "react-reveal/Fade";
+import Image from "next/image";
 import Name from "../components/Name";
 import Layout from "../components/Layout";
 import styles from "../styles/pages/Home.module.css";
-import heroBkg from "../public/heroBG.jpg"
+import heroBkg from "../public/heroBG.jpg";
 
 const Home: NextPage = () => {
   return (
@@ -13,34 +12,38 @@ const Home: NextPage = () => {
       <Head>
         <title>Evan&apos;s Website</title>
         <meta
-          name='description'
-          content='Evan Zaplachinskis Personal Website'
+          name="description"
+          content="Evan Zaplachinskis Personal Website"
         />
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <header className={styles.appHeader}>
-          <Image src={heroBkg} layout="fill" alt="bkg-img" objectFit="cover" placeholder="blur" />
+          <Image
+            src={heroBkg}
+            layout="fill"
+            alt="bkg-img"
+            objectFit="cover"
+            placeholder="blur"
+          />
           <Name />
         </header>
-        <Layout footerText='cool Footer' stickyOffset={0.8} >
-          <Fade bottom>
-            <div className={styles.wrapper} style={{ marginTop: "15vh" }}>
-              <div id={styles.aside1}></div>
-              <div className={styles.content}>
-                <h3>Header</h3>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Voluptas voluptate magni reiciendis porro at illo aspernatur
-                  accusamus autem cum ab quisquam esse provident necessitatibus,
-                  ut, adipisci explicabo, possimus recusandae repudiandae.
-                </p>
-              </div>
-              <div style={{ paddingBottom: "100vh" }} />
-              <div id={styles.aside2}></div>
+        <Layout footerText="cool Footer" stickyOffset={0.8}>
+          <div className={styles.wrapper} style={{ marginTop: "15vh" }}>
+            <div id={styles.aside1}></div>
+            <div className={styles.content}>
+              <h3>Header</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptas voluptate magni reiciendis porro at illo aspernatur
+                accusamus autem cum ab quisquam esse provident necessitatibus,
+                ut, adipisci explicabo, possimus recusandae repudiandae.
+              </p>
             </div>
-          </Fade>
+            <div style={{ paddingBottom: "100vh" }} />
+            <div id={styles.aside2}></div>
+          </div>
         </Layout>
       </main>
     </div>
