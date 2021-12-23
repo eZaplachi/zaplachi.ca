7c954737fd6086215dbc7c7297d1e5f447e47673
eZaplachi <evan@zaplachi.ca>
2021-12-22 22:54:18 -0700

    updated projects scaling

--- styles/pages/projects/Projects.module.css.d.ts
+++ styles/pages/projects/Projects.module.css.d.ts
@@ -3,12 +3,6 @@ declare const styles: {
   readonly "aside1": string;
   readonly "aside2": string;
   readonly "cardContainer": string;
-  readonly "card1": string;
-  readonly "card2": string;
-  readonly "card3": string;
-  readonly "card4": string;
-  readonly "card5": string;
-  readonly "card6": string;
 };
 export = styles;
 
