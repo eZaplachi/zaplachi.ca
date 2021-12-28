e17d5f3ad58e176d55ec558e37229aa5e3d56dc5
eZaplachi <evan@zaplachi.ca>
2021-12-28 00:57:10 -0700

    better error msg

---
+++ styles/pages/404.module.css.d.ts
@@ -0,0 +1,10 @@
+declare const styles: {
+  readonly "container": string;
+  readonly "aside1": string;
+  readonly "aside2": string;
+  readonly "errorMsg": string;
+  readonly "title": string;
+  readonly "info": string;
+};
+export = styles;
+
