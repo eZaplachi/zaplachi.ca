98c4439ffba13c4736f31b375cb31c3772fdd1f9
eZaplachi <evan@zaplachi.ca>
2022-03-12 12:02:47 -0700

    comment out testing code

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,14 +1,14 @@
 import { GetStaticProps } from "next";
-import { getWorkspaces } from "../lib/contentfulHelper";
+// import { getWorkspaces } from "../lib/contentfulHelper";
 import styles from "../styles/components/Dropdown.module.css";
 
-export const getStaticProps: GetStaticProps = async () => {
-  const data = await getWorkspaces();
+// export const getStaticProps: GetStaticProps = async () => {
+//   const data = await getWorkspaces();
 
-  return {
-    props: { myWorkspaces: data.workSpacesCollection.items },
-  };
-};
+//   return {
+//     props: { myWorkspaces: data.workSpacesCollection.items },
+//   };
+// };
 
 const Dropdown = ({ myWorkspaces }: any) => {
   return (
