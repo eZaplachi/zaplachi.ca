fc1bd371144f11b8bd0d1f5669e394dac50fe7c1
eZaplachi <evan@zaplachi.ca>
2022-03-30 01:11:25 -0600

    Basic page transition -- broke name

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { ReactChild, useState } from "react";
 import Head from "next/head";
 import useScroll from "./useScroll";
@@ -28,20 +28,16 @@ const Layout = (props: {
 
   return (
     <section lang="en">
+      <Head>
+        <title>&nbsp;Evan&apos;s Portfolio {headTitle}</title>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="author" content="Evan Zaplachinski" />
+        <meta name="description" content={props.description} />
+        <meta name="keywords" content={props.keywords} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <AnimatePresence exitBeforeEnter>
-        <Head>
-          <title>&nbsp;Evan&apos;s Portfolio {headTitle}</title>
-          <meta charSet="UTF-8" />
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-          <meta name="author" content="Evan Zaplachinski" />
-          <meta name="description" content={props.description} />
-          <meta name="keywords" content={props.keywords} />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-
         <div className={styles.navbar} id={scrolled ? styles.scrolled : ""}>
           <Navbar />
         </div>
@@ -51,7 +47,7 @@ const Layout = (props: {
           className={styles.wrapper}
           id={scrolled ? styles.scrolledSpace : ""}
         >
-          <div className={styles.content}>{props.children}</div>
+          <motion.div className={styles.content} exit={{opacity: 0}} initial={{opacity: 0}} animate={{opacity: 1}}>{props.children}</motion.div>
         </div>
         <footer id={styles.footerSpace}>
           <Footer text={props.footerText} />
